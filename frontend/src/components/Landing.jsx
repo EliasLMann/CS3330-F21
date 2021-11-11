a8ddc03a7e43f4e5738efe64f3caea098f6354e2
leongkkevin <kkleong@smu.edu>
2021-11-11 11:29:58 -0600

    Changed landing page and added links

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -5,25 +5,29 @@ import Button from 'react-bootstrap/Button';
 import logo from './PopStopLogo.png';
 import './landing.css'
 
+import { Link } from 'react-router-dom';
+
 export class Login extends React.Component {
 
     render() {
         return <>
             <img id="logoPic" class="card-img-top" src={logo}/>
             <div class="d-flex flex-row justify-content-center">
-                <Card class="p-2 mx-sm" style={{ width: '18rem' }}>
+                <Card class="p-2 mx-l" style={{ width: '18rem' }}>
                     <Card.Body>
-                        <Card.Title class="text-primary">Restaurant Login</Card.Title>
-                        <Card.Text class="text-center text-muted">
+                        <Card.Title class="text-center text-primary mx-auto">Login to PopStop</Card.Title>
+                        {/* <Card.Text class="text-center text-muted">
                             <h5>If you own/represent a restaurant, follow the links below </h5>
-                        </Card.Text>
-                        <Button variant="primary">Login</Button>
-                        <br />
-                        <Button variant="primary">Register</Button>
+                        </Card.Text> */}
+                        <div className="row">
+                            <Link className="btn btn-primary col" to='/login'>Login</Link>
+                            <br />
+                            <Link className="btn btn-primary col" to='/register'>Register</Link>
+                        </div>
                     </Card.Body>
                 </Card>
 
-                <Card class="p-2 mx-sm" style={{ width: '18rem' }}>
+                {/* <Card class="p-2 mx-sm" style={{ width: '18rem' }}>
                     <Card.Body>
                         <Card.Title class="text-primary">Diner Login</Card.Title>
                         <Card.Text class="text-center text-muted">
@@ -33,7 +37,7 @@ export class Login extends React.Component {
                         <br />
                         <Button variant="primary">Register</Button>
                     </Card.Body>
-                </Card>
+                </Card> */}
             </div>
 
         </>;
