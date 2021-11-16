31a98f12ed13bcdf1794fd1e73f5aa04fa6ee0dd
melwri <melaniew@smu.edu>
2021-11-15 21:17:00 -0600

    Added getmealtype route

--- backend/routes.js
+++ backend/routes.js
@@ -95,4 +95,33 @@ module.exports = function routes(app, logger) {
       }
     });
   });
+
+    // GET /getmealtype
+    app.get('/getmealtype', (req, res) => {
+      // obtain a connection from our pool of connections
+      pool.getConnection(function (err, connection){
+        if(err){
+          // if there is an issue obtaining a connection, release the connection instance and log the error
+          logger.error('Problem obtaining MySQL connection',err)
+          res.status(400).send('Problem obtaining MySQL connection'); 
+        } else {
+          // if there is no issue obtaining a connection, execute query and release connection
+          connection.query('SELECT mealType FROM `PopStop`.`MenuItem`', function (err, rows, fields) {
+            connection.release();
+            if (err) {
+              logger.error("Error while fetching values: \n", err);
+              res.status(400).json({
+                "data": [],
+                "error": "Error obtaining values"
+              })
+            } else {
+              res.status(200).json({
+                "data": rows
+              });
+            }
+          });
+        }
+      });
+    });
+
 }
