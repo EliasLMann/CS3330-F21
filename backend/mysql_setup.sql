a8bfe8e42486d9b59dadad9fa70df2a9343984fe
AriyanK <akumaraswamy@smu.edu>
2021-11-15 21:12:46 -0600

    Changed database values

--- backend/mysql_setup.sql
+++ backend/mysql_setup.sql
@@ -12,7 +12,11 @@ CREATE TABLE `db`.`test_table` (
     UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE
 );
 
+<<<<<<< Updated upstream
 CREATE TABLE `db`.`Restaurant` (
+=======
+CREATE TABLE `PopStop`.`Restaurant` (
+>>>>>>> Stashed changes
 	`restaurantID` INT NOT NULL AUTO_INCREMENT, PRIMARY KEY(restaurantID),
 	`restaurantName` varchar(255),
 	`menuID` INT(11),
@@ -22,6 +26,7 @@ CREATE TABLE `db`.`Restaurant` (
 	`description` varchar(1000),
 	`cuisineType` varchar(255),
 	`website` varchar(255),
+<<<<<<< Updated upstream
 	`sponsored` boolean,
 	`socialMediaName` varchar(255),
 	`socialMediaURL` varchar(255));
@@ -29,11 +34,30 @@ CREATE TABLE `db`.`Restaurant` (
 -- insert sample entry
 INSERT INTO `db`.`test_table` (`value`) VALUES ('Sample Value');
 INSERT INTO `db`.`Restaurant` (`restaurantName`, `menuID`, `ownerID`, `location`, `hours`, `description`, `cuisineType`, 
+=======
+	`sponsored` bit,
+	`socialMediaName` varchar(255),
+	`socialMediaURL` varchar(255));
+
+
+-- insert sample entry
+INSERT INTO `PopStop`.`test_table` (`value`) VALUES ('Sample Value');
+INSERT INTO `PopStop`.`Restaurant` (`restaurantName`, `menuID`, `ownerID`, `location`, `hours`, `description`, `cuisineType`, 
+>>>>>>> Stashed changes
 							`website`, `sponsored`, `socialMediaName`, `socialMediaURL`)
 						VALUES
 								('testName', 1, 1, 'testLoc', 'testHours', 'testDesc', 'testCuisine', 'testWeb',
 								false , 'testSocName', 'testSocURL');
+<<<<<<< Updated upstream
 							
+=======
+
+alter table Restaurant
+add constraint menuID
+foreign key(menuID)
+references Menu (menuID);
+
+>>>>>>> Stashed changes
 -- select * from Restaurant;
 
 
