2e4e77e9a054b8dfc191853fbb370cb85c0c262e
leongkkevin <kkleong@smu.edu>
2021-11-17 20:13:02 -0600

    Styled register page

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -12,32 +12,19 @@ export class Login extends React.Component {
     render() {
         return <>
             <img id="logoPic" class="card-img-top" src={logo}/>
+            <h1 className="font-weight-bold">Welcome to PopStop!</h1>
             <div class="d-flex flex-row justify-content-center">
-                <Card class="p-2 mx-l" style={{ width: '18rem' }}>
+                <Card class="p-2 mx-l px-5" style={{ width: '30rem', large:'20rem'}}>
                     <Card.Body>
-                        <Card.Title class="text-center text-primary mx-auto">Login to PopStop</Card.Title>
-                        {/* <Card.Text class="text-center text-muted">
-                            <h5>If you own/represent a restaurant, follow the links below </h5>
-                        </Card.Text> */}
-                        <div className="row">
-                            <Link className="btn btn-primary col" to='/login'>Login</Link>
+                        <Card.Title class="text-center text-primary mx-auto font-weight-bold">Login to PopStop</Card.Title>
+                        <br />
+                        <div className="row px-3">
+                            <Link className="btn btn-primary col mx-3" to='/login'>Login</Link>
                             <br />
                             <Link className="btn btn-primary col" to='/register'>Register</Link>
                         </div>
                     </Card.Body>
                 </Card>
-
-                {/* <Card class="p-2 mx-sm" style={{ width: '18rem' }}>
-                    <Card.Body>
-                        <Card.Title class="text-primary">Diner Login</Card.Title>
-                        <Card.Text class="text-center text-muted">
-                            <h5>If you are a customer looking for restaurants, follow the links below</h5>
-                        </Card.Text>
-                        <Button variant="primary">Login</Button>
-                        <br />
-                        <Button variant="primary">Register</Button>
-                    </Card.Body>
-                </Card> */}
             </div>
 
         </>;
