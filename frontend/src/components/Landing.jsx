2213bc534aa7d661e3a4c68f6c4bd1a777f326b6
leongkkevin <kkleong@smu.edu>
2021-11-18 15:47:09 -0600

    New landing

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -11,7 +11,12 @@ export class Login extends React.Component {
 
     render() {
         return <>
+<<<<<<< Updated upstream
             <img id="logoPic" class="card-img-top" src={logo}/>
+=======
+            <img id="background" src="https://wallpaperaccess.com/full/4895186.jpg" style={{filter: 'blur(2px)'}}/>
+            <img id="logoPic" className="card-img-top justify-content-left" src={logo} />
+>>>>>>> Stashed changes
             <div class="d-flex flex-row justify-content-center">
                 <Card class="p-2 mx-l" style={{ width: '18rem' }}>
                     <Card.Body>
