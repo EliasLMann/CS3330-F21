d4f173586c3e2449cb1ce9ff3faef7c603d346ce
leongkkevin <kkleong@smu.edu>
2021-11-24 15:26:27 -0600

    Added menu functions

--- frontend/src/components/AddMenu.jsx
+++ frontend/src/components/AddMenu.jsx
@@ -29,19 +29,40 @@ export class AddMenu extends React.Component{
             <h1>Add or Edit your menu!</h1> 
 
             {
-                this.state.menu.map(menuItem => <div className="card w-75 mx-auto mb-3" key={menuItem.id}>
-                    <span>Menu Item Name:</span>
-                    <input type="text"></input>
-                    <span>Menu Item Price:</span>
-                    <input type="text"></input>
-                    <span>Menu Item Description:</span>
-                    <textarea></textarea>
+                this.state.menu.map( (menuItem, i) => <div className="card w-75 mx-auto mb-3" key={i}>
+                    <span className="card-header">Menu Item #{i}</span>
+
+                    <form className="form-group card-body p-2">
+                        <div className="row">
+
+                            <div className="col-md-7">
+                                <label>Menu Item Name:</label>
+                                <input type="text"></input>
+                            </div>
+                            <div className="col-md-5">
+                                <label>Menu Item Price: $</label>
+                                <input className="w-25" type="text"></input>
+                            </div>
+
+                        </div>
+
+                        <br/>
+
+                        <div className="d-flex">
+                            <label className="text-center">Menu Item Description:</label>
+                            <textarea className="w-75 form-control"></textarea>
+                        </div>
+                        
+                    </form>
+
                 </div>)
             }
 
-            <button className="btn btn-secondary mx-auto" onClick={() => this.addItem()}>Add Item</button>
-            <br/>
-            <button className="btn btn-primary">Save Menu</button>
+            <div className="mx-auto d-flex justify-content-center">
+                <button className="btn btn-secondary" onClick={() => this.addItem()}>Add Item</button>
+                <br/>
+                <button className="btn btn-primary">Save Menu</button>
+            </div>
 
 
         </>
