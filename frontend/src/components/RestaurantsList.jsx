b9b777654409427adcd923808373c8f31bb96d8a
leongkkevin <kkleong@smu.edu>
2021-11-28 17:16:39 -0600

    Updated some Search functionality

--- frontend/src/components/RestaurantsList.jsx
+++ frontend/src/components/RestaurantsList.jsx
@@ -1,5 +1,14 @@
-export const RestaurantList = props => {
-    if (!props.accounts) {
+export const RestaurantsList = props => {
+    if (!props.restaurants) {
         return <div>Loading...</div>
     }
+    return <>
+        {
+            props.restaurants.map(restaurant => <div>
+                <h1>{restaurant.name}</h1>
+            </div>)
+    
+
+        }
+    </>
 }
--- frontend/src/components/SearchPage.jsx
+++ frontend/src/components/SearchPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Dropdown } from "react-bootstrap";
 import { Header } from "./Header";
-import RestaurantList from "./RestaurantList";
+import {RestaurantsList} from "./RestaurantsList";
 
 import { useState, useEffect } from "react";
 
@@ -11,6 +11,7 @@ import { RestaurantList } from "./RestaurantsList";
 export const SearchPage = props => {
 
     const [ restaurants, setRestaurants ] = useState(undefined);
+    const [ searchFilter, setSearchFilter] = useState("Search By...");
 
     const userRepo = new UserRepository();
 
@@ -30,22 +31,23 @@ export const SearchPage = props => {
             <div class="input-group mx-auto p-3 w-100 align-middle">
                 <Dropdown>
                     <Dropdown.Toggle variant="outline-secondary" id="dropdown-basic">
-                        Search By...
+                        {searchFilter}
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item>Average Price</Dropdown.Item>
-                        <Dropdown.Item>Cuisine Type</Dropdown.Item>
-                        <Dropdown.Item>Location</Dropdown.Item>
-                        <Dropdown.Item>Distance</Dropdown.Item>
-                        <Dropdown.Item>Zip Code</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Show All")}>Show All</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Average Price")}>Average Price</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Cuisine Type")}>Cuisine Type</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Location")}>Location</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Distance")}>Distance</Dropdown.Item>
+                        <Dropdown.Item onClick={(e) => setSearchFilter("Zip Code")}>Zip Code</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <input type="text" class="form-control align-middle" placeholder="Find your next destination..."/>
                 <button className="btn btn-secondary h-25">Search</button>
             </div>
         </div>
-        <RestaurantList restaurants = {restaurants} />
+        <RestaurantsList restaurants = {restaurants} />
     </>
     
 
